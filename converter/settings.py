937d28b960406351f2e3be4f39c3d9ae690c689f
abhijit <abhijitlimited1@gmail.com>
2025-06-07 19:41:43 +0530

    changes in setting file

--- converter/settings.py
+++ converter/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-qx)j8!wl#n_7-$@wnj$6&@hfpeuli3ztdoi!jk4v=#7)dn&^2)'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['jpg-converter-backend.onrender.com']
 
 # CORS settings
 CORS_ALLOWED_ORIGINS = [
